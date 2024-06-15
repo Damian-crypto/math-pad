2156be240558b611041e8c00023c5546e04e0c64
Damian <www.damianchamel@gmail.com>
2024-06-15 20:36:07 +0530

    fixed table bugs

--- src/dtable/dtable.js
+++ src/dtable/dtable.js
@@ -7,6 +7,8 @@ function createCell(id, i, j, rows) {
 	var input = document.createElement("input");
 	input.classList.add("tbl_input");
 	input.id = `cell_${id}_${rows * i + j}`;
+
+	// Event handling of this cell
 	input.addEventListener("input", () => {
 		activeCell = `cell_${id}_${rows * i + j}`;
 		adjustWidth();
@@ -19,30 +21,28 @@ function createCell(id, i, j, rows) {
 }
 
 function createTable(id, rows = 3, cols = 5) {
+	var body = document.getElementsByTagName("body");
 	var table = document.createElement("table");
 	table.classList.add("d_table");
 	table.id = `table_${id}`;
-	for (let i = 0; i < rows; i++) {
-		var row = document.createElement("tr");
-		row.id = `row_${id}_${i}`;
-		for (let j = 0; j < cols; j++) {
-			var col = document.createElement("td");
-			col.id = `col_${id}_${i}`;
-			var input = createCell(id, i, j, rows);
-			col.appendChild(input);
-			row.appendChild(col);
-		}
-		table.appendChild(row);
-	}
+	body[0].appendChild(table);
 
 	table.addEventListener("click", () => {
 		activeTable = id;
 	});
 
-	tableData[id] = [ rows, cols ];
 	activeTable = id;
+	tableData[id] = [ 0, 0 ];
 
-    return table;
+	for (let i = 0; i < rows; i++) {
+		addRow();
+	}
+	// This is because at the time there are 0 columns (adding rows won't add columns)
+	for (let i = 0; i < cols; i++) {
+		addColumn();
+	}
+
+	return table;
 }
 
 function addTable() {
@@ -55,8 +55,8 @@ function addColumn() {
 
 		for (let i = 0; i < rows; i++) {
 			var col = document.createElement("td");
-			col.id = `col_${activeTable}_${cols + i}`;
-			var input = createCell(activeTable, i, cols + 1, rows);
+			col.id = `col_${activeTable}_${cols + 1}`;
+			var input = createCell(activeTable, i, cols, rows);
 			col.appendChild(input);
 
 			var row = document.getElementById(`row_${activeTable}_${i}`);
@@ -71,12 +71,12 @@ function addRow() {
 	if (activeTable !== -1) {
 		var [ rows, cols ] = tableData[activeTable];
 		var row = document.createElement("tr");
-		row.id = `row_${activeTable}_${rows + 1}`;
+		row.id = `row_${activeTable}_${rows}`;
 
-		for (let i = 0; i < cols; i++) {
+		for (let i = 1; i <= cols; i++) {
 			var col = document.createElement("td");
-			col.id = `col_${activeTable}_${cols + i}`;
-			var input = createCell(activeTable, rows + 1, i, rows);
+			col.id = `col_${activeTable}_${i}`;
+			var input = createCell(activeTable, rows, i, rows);
 			col.appendChild(input);
 
 			row.appendChild(col);
@@ -85,6 +85,31 @@ function addRow() {
 		var table = document.getElementById(`table_${activeTable}`);
 		table.appendChild(row);
 		tableData[activeTable] = [rows + 1, cols];
+	}
+}
+
+function deleteRow() {
+	if (activeTable !== -1) {
+		var [ rows, cols ] = tableData[activeTable];
+		var row = document.getElementById(`row_${activeTable}_${rows - 1}`)
+
+		row.remove();
+
+		tableData[activeTable] = [rows - 1, cols];
+	}
+}
+
+function deleteColumn() {
+	if (activeTable !== -1) {
+		var [ rows, cols ] = tableData[activeTable];
+		
+		for (let i = 0; i < rows; i++) {
+			var row = document.getElementById(`row_${activeTable}_${i}`)
+			var col = row.querySelector(`#col_${activeTable}_${cols}`);
+			col.remove();
+		}
+
+		tableData[activeTable] = [rows, cols - 1];
 	}
 }
 
